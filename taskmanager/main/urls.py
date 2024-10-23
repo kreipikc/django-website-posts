16ff2142d5118e28728ddb852bd6bdd1f9671eb2
kreipikc <basmenov5kir@gmail.com>
2024-10-23 21:38:15 +0300

    add login-registration and logout

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
+    path('u/registration', views.registration, name='registration'),
+    path('u/login', views.login_view, name='login'),
+    path('u/logout', views.logout_view, name='logout')
 ]
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,6 +1,44 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'main/login.html', {'form': form})
+    else:
+        form = AuthenticationForm()
+    return render(request, 'main/login.html', {'form': form})
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # получаем имя пользователя и пароль из формы
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            # выполняем аутентификацию
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'main/signup.html', {'form': form})
+    else:
+        form = UserCreationForm()
+    return render(request, 'main/signup.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
 def index(request):
     return render(request, 'main/index.html')
 
