50732849dea02f50d903e36677ab485f87ac1fc6
kreipikc <basmenov5kir@gmail.com>
2024-10-26 17:47:09 +0300

    Added the ability to create, as well as the author of the post to edit and delete it

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('u/registration', views.registration, name='registration'),
     path('u/login', views.login_view, name='login'),
-    path('u/logout', views.logout_view, name='logout')
+    path('u/logout', views.logout_view, name='logout'),
+    path('create/', views.create_post, name='create_post'),
+    path('<int:post_id>/edit/', views.edit_post, name='edit_post'),
+    path('<int:post_id>/delete/', views.delete_post, name='delete_post'),
 ]
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,6 +1,52 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from .forms import PostForm
+from .models import Post
+
+
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('/') # Перенаправляем на список постов
+    else:
+        form = PostForm()
+    return render(request, 'main/create_post.html', {'form': form})
+
+@login_required
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author == request.user:
+        if request.method == 'POST':
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                form.save()
+                return redirect('/')
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'main/edit_post.html', {'form': form})
+    else:
+        return redirect('/')
+
+@login_required
+def delete_post(request, post_id):
+    if request.method == 'POST':
+        try:
+            post = post = get_object_or_404(Post, pk=post_id)
+            post.delete()
+            messages.success(request, 'Пост успешно удален.')
+            return redirect('/')
+        except Post.DoesNotExist:
+            messages.error(request, 'Пост не найден.')
+            return redirect('/')
+    return redirect('/')
 
 
 def login_view(request):
@@ -30,17 +76,21 @@ def registration(request):
             login(request, user)
             return redirect('/')
         else:
-            return render(request, 'main/signup.html', {'form': form})
+            return render(request, 'main/registration.html', {'form': form})
     else:
         form = UserCreationForm()
-    return render(request, 'main/signup.html', {'form': form})
+    return render(request, 'main/registration.html', {'form': form})
 
 def logout_view(request):
     logout(request)
     return redirect('/')
 
 def index(request):
-    return render(request, 'main/index.html')
+    posts = Post.objects.order_by('-created_at')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'main/index.html', context)
 
 def about(request):
     return render(request, 'main/about.html')
